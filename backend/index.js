94948c7ad7525d6ac4b81a66eefc26d7a221f3a0
Akshat <110756105+Akshatkanth@users.noreply.github.com>
2025-04-13 20:54:22 +0530

    changes to index.js

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,15 @@ app.use(cors({ origin: true }));
 
 app.post("/authenticate", async (req, res) => {
   const { username } = req.body;
-  return res.json({ username: username, secret: "sha256..." });
+
+  try{
+    const r = await axios.put =
+  }catch(e){
+
+  }
+
+  sec-c-MWFjZDVmODEtNzYxZS00OGU1LTllZTktMTlkODBjYzdmNzNh
+  return res.json({ username: username, secret: "pass1234" });
 });
 
 app.listen(3001);
